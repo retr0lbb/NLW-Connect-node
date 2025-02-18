8d7232014c1054220442d4a3a8d3c17d26a0f038
retr0lbb <retr0lbb@gmail.com>
2025-02-18 01:18:25 -0300

    feat: add env type safety

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import fastify from "fastify";
 import { fastifyCors } from "@fastify/cors";
-import {
-	validatorCompiler,
-	serializerCompiler,
-	type ZodTypeProvider,
-	jsonSchemaTransform,
-} from "fastify-type-provider-zod";
-import z from "zod";
 import { fastifySwagger } from "@fastify/swagger";
 import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import fastify from "fastify";
+import {
+	type ZodTypeProvider,
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+} from "fastify-type-provider-zod";
+import { env } from "./env";
 import { subscribeToEventRoute } from "./routes/subscribe-to-event";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -34,7 +34,7 @@ app.register(subscribeToEventRoute);
 
 app
 	.listen({
-		port: 3333,
+		port: env.PORT,
 	})
 	.then(() => {
 		console.log("server ready");
